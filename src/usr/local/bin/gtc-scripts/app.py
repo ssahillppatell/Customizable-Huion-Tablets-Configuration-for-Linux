e6a91ecfd2b7e17a55ee1dac632eef5bf423c7c1
Sahil Patel <ssahillppatell@gmail.com>
2021-05-06 11:44:23 +0530

    Persistent Configuration

--- src/usr/local/bin/gtc-scripts/app.py
+++ src/usr/local/bin/gtc-scripts/app.py
@@ -24,11 +24,19 @@ if os.path.exists(f"/usr/share/X11/xorg.conf.d/99-huion{model}.conf"):
 	args = ''
 	for key, value in newConfig.items():
 		args += f'"{key}:{value}" '
-	
+
+	with open(f'/home/pagal/.graphic-tab-config', "w+") as fh:
+		fh.write(args)
+	os.chmod('/home/pagal/.graphic-tab-config', 0o777)
+
 	print(args)
 	subprocess.run(['bash', './setButtons.sh', args])
 else:
 	with open(f"/usr/share/X11/xorg.conf.d/99-huion{model}.conf", "w") as fh:
 		fh.write(f'Section "InputClass"\n\tIdentifier "Huion tablets with Wacom driver"\n\tMatchUSBID "256c:006d*"\n\tMatchIsTablet "true"\n\tMatchDevicePath "/dev/input/event*"\n\tDriver "wacom"\nEndSection\n\t')
 	
+	with open(f'/etc/init.d/gtc-onboot.sh', "w") as fh:
+		fh.write('bash /usr/local/bin/gtc-scripts/setButtons.sh "$(cat /home/pagal/.graphic-tab-config)"')
+	
+	os.system('sudo chmod +x /etc/init.d/gtc-onboot.sh')
 	os.system('sudo systemctl restart display-manager')
